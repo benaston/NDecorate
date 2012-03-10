26e7e740e1e661cad5a29918fa5969af7bcb0d1e
Ben Aston <ben.aston@justgiving.com>
2012-03-10 14:17:35 +0000

    Header text correction.

--- src/NDecorate.Test.Fast/Feature.cs
+++ src/NDecorate.Test.Fast/Feature.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2012, Ben Aston (ben@bj.ma).
 // 
-// This file is part of NBasicExtensionMethod.
+// This file is part of NDecorate.
 // 
-// NBasicExtensionMethod is free software: you can redistribute it and/or modify
+// NDecorate is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// NBasicExtensionMethod is distributed in the hope that it will be useful,
+// NDecorate is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 // 
 // You should have received a copy of the GNU Lesser General Public License
-// along with NBasicExtensionMethod. If not, see <http://www.gnu.org/licenses/>.
+// along with NDecorate. If not, see <http://www.gnu.org/licenses/>.
 
 namespace NDecorate.Test.Fast
 {
